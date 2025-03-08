62d3db9f85242da3b0903ace918cbe760fc88f87
Fabian <fabianmtovar26@gmail.com>
2025-03-08 11:34:05 -0400

    clase usuario agregada

---
+++ usuario.py
@@ -0,0 +1,8 @@
+class Usuario:
+    def __init__(self, nombre, peso, altura, edad, genero, nivel_ejercicio):
+        self.nombre = nombre
+        self.peso = peso
+        self.altura = altura
+        self.edad = edad
+        self.genero = genero
+        self.nivel_ejercicio = nivel_ejercicio
