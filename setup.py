c1398da3dda1f10d416d0eb97579f354a292a2a3
Andres Roche <hewsanchez@gmail.com>
2025-03-12 16:26:49 -0400

    descargar csv

--- setup.py
+++ setup.py
@@ -1,14 +1,24 @@
 import kagglehub
 import os
+import shutil
 
 # Especificar el nombre o ruta de la carpeta
 nombre_carpeta = "dataset"
+
+# Especifica el nombre del dataset en Kaggle
+dataset_path = "irkaal/foodcom-recipes-and-reviews"
+
+# Download latest version
+path = kagglehub.dataset_download(dataset_path)
 
 # Crear la carpeta si no existe
 if not os.path.exists(nombre_carpeta):
     os.makedirs(nombre_carpeta)
 
-# Download latest version
-path = kagglehub.dataset_download("/irkaal/foodcom-recipes-and-reviews", destination="dataset")
+# Mover el archivo descargado a la carpeta deseada
+for filename in os.listdir(downloaded_path):
+    shutil.move(os.path.join(downloaded_path, filename), nombre_carpeta)
+
+print(f"Dataset descargado y movido a la carpeta: {nombre_carpeta}")
 
 print("Path to dataset files:", path)
